ed9716d875f130a3b7a87eb1669020e99b9d96e3
Sambit0510 <86628293+Sambit0510@users.noreply.github.com>
2021-10-23 23:13:12 +0530

    Create HeaderArray.h

---
+++ HeaderArray.h
@@ -0,0 +1,35 @@
+#ifndef HEADERARRAY_H_INCLUDED
+#define HEADERARRAY_H_INCLUDED
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+InsertItemAtLast(int , int , int );
+ InsertItemAtFirst(int , int , int );
+ InsertItemAtIndex(int , int , int , int );
+ DeleteItemFromLast(int , int , int );
+ DeleteItemfromFirst(int , int , int );
+ DeleteItemFromIndex(int , int , int , int);
+ FindItemUnsorted(int, int , int);
+ FindItemSorted(int , int , int );
+ SortArray(int , int);
+ ExtractSubest(int , int , int , int );
+ DeleteSubset(int , int , int , int );
+ SplitIntoTwoArray(int , int );
+ CloneArray(int , int , int );
+ ShiftLeftArray(int , int , int );
+ ShiftRightArray(int , int , int );
+ RotateArrayClockwise(int , int , int );
+ RotateArrayAntiClockwise(int, int , int );
+ FindMin(int , int );
+ FindMax(int , int );
+ FillArrayPseudoRandom(int, int );
+ FillArrayTrueRandom(int , int );
+ IncreaseArraySize(int , int , int );
+ SetArrayToZero(int , int );
+ DeleteAllItemOfArray(int, int );
+ DeleteArray(int , int );
+ AllocateArray(int , int );
+#endif // HEADERARRAY_H_INCLUDED
